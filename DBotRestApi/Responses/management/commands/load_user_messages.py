10fb41a166bb2b609fa926e6ff825e8adcbcbd5d
Miecznikk <dominikgdw@gmail.com>
2024-04-19 19:16:01 +0200

    loading messages script

---
+++ DBotRestApi/Responses/management/commands/load_user_messages.py
@@ -0,0 +1,31 @@
+import os
+
+from django.core.management.base import BaseCommand
+
+from DBotRestApi import settings
+from Responses.models import DefaultUserRelatedMessage
+from Members.models import Member
+from django.apps import apps
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('member_name')
+        parser.add_argument('file_name')
+
+    def handle(self, *args, **options):
+        member_name = options['member_name']
+        file_name = os.path.join(f'{settings.BASE_DIR}/Responses/management/commands/', options['file_name'])
+        try:
+            member = Member.objects.get(name=member_name)
+            try:
+                with open(file_name, 'r') as file:
+                    lines = [line[:-1] for line in file]
+                for line in lines:
+                    DefaultUserRelatedMessage.objects.create(user=member, description=line)
+            except FileNotFoundError:
+                print(f"File {file_name} not found")
+                return
+        except Member.DoesNotExist:
+            print(f"Member {member_name} does not exist")
+            return
